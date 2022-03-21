9c0b9a5ed0804718561cc605caa5f018f094f97c
Johanahsberg <56584255+Johanahsberg@users.noreply.github.com>
2022-03-21 08:51:15 +0100

    Cleanup

--- tdc-generator/src/App.js
+++ tdc-generator/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h1>TDC Generator</h1>
+        <form>
+          <div className="form" id="form1">
+            <input
+              className="inputText"
+              type="text"
+              placeholder="Theme"
+            ></input>
+            <input
+              className="inputText"
+              type="text"
+              placeholder="# of participants"
+            ></input>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
